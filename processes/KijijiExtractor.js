0c8a1bb0189936588ceeaf856ff1ebeb2ba37739
Philippe Richard <philippe.richard93@gmail.com>
2016-01-23 14:31:17 -0500

    update Kijiji-extractor

--- processes/KijijiExtractor.js
+++ processes/KijijiExtractor.js
@@ -1,30 +1,68 @@
 var Q = require('q');
 var _ = require('lodash');
+var kijiji = require('kijiji-scraper');
+
+var request = {
+  region : 80002,
+  target : ['kijiji'],
+  languages : {
+    french : {
+      keywords : ['Cle', 'USB', '16GB'],
+      tags : ['kingston'],
+      categories : ['electronique', 'composantes', 'informatique']
+    },
+    english : {
+      keywords : ['USB', 'Key', '16GB'],
+      tags : ['kingston'],
+      categories : ['electronics', 'components', 'computer']
+    }
+  }
+}
+
+function keywordsBuilder(request, language){
+
+  var keywords = request.languages[language].keywords;
+
+  var queryString = keywords[0];
+
+  for(var i = 1; i < keywords.length; ++i){
+    queryString += '+';
+    queryString += keywords[i];
+  }
+
+  return queryString;
+
+}
 
 function normalize(data) {
 
-  return data + ' normalized';
+  return data;
 }
 
-function extract() {
+function extract(request) {
+
   var deferred = Q.defer();
 
-  var fetchedData = ['lol', 'lol2', 'lol3'];
-  var cleanedData = [];
+  kijiji.query({'locationId':request.region}, {'keywords':keywordsBuilder(request, 'french')}, function(err, ads) {
 
-  _.each(fetchedData, function(item, index, array) {
+    var cleanedData = [];
 
-    cleanedData[index] = normalize(item);
+    _.each(ads, function(ad, index, array) {
 
-    if(index == array.length - 1) deferred.resolve(cleanedData);
+      cleanedData[index] = normalize(ad);
+
+      if(index == array.length - 1) deferred.resolve(cleanedData);
+    });
+
   });
 
   return deferred.promise;
+
 }
 
 module.exports = {
   extract: extract
 }
 
-extract()
+extract(request)
   .then(function(data) {console.log(data)});
