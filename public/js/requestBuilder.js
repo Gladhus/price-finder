a18a2b083e75aef73f710acc807f5b6a42bb5a70
Francis <francis.beauchampsc@gmail.com>
2016-01-23 21:19:44 -0500

    Fixes indentaiton

--- public/js/requestBuilder.js
+++ public/js/requestBuilder.js
@@ -17,76 +17,76 @@ Dropzone.options.myDropzone = {
 }; */
 
 function buildClarifaiData() {
-	var file    = document.querySelector('input[type=file]').files[0];
+  var file    = document.querySelector('input[type=file]').files[0];
   var reader  = new FileReader();
 
   if (file) {
     reader.readAsDataURL(file);
   }
 
-	sendClarifaiRequest(img);
+  sendClarifaiRequest(img);
 }
 
 function updateResult(data, textStatus, jqXHR) {
-	console.log('Allo');
+  console.log('Allo');
 }
 
 function sendClarifaiRequest(img) {
-	$.ajax({
-	  type: "POST",
-	  url: urlClarifai,
-	  headers: { "grant_type" : clarifaiCredentials.grant_type.toString() },
-	  data: { "client_id" : clarifaiCredentials.client_id.toString(),
-	  				"client_secret" : clarifaiCredentials.client_secret.toString() },
-	  success: updateResult,
-	  dataType: "json"
-	});
+  $.ajax({
+    type: "POST",
+    url: urlClarifai,
+    headers: { "grant_type" : clarifaiCredentials.grant_type.toString() },
+    data: { 
+      "client_id" : clarifaiCredentials.client_id.toString(),
+      "client_secret" : clarifaiCredentials.client_secret.toString() 
+    },
+    success: updateResult,
+    dataType: "json"
+  });
 }
 
 function getTagsInputValues(selector) {
-	tagNames = [ ];
-	$(selector).find('.bootstrap-tagsinput').find(".tag").each(function(tagIndex) {
-		tagNames.push($(this).text())
-	});
+  tagNames = [ ];
+  $(selector).find('.bootstrap-tagsinput').find(".tag").each(function(tagIndex) {
+    tagNames.push($(this).text())
+  });
 
  return tagNames;
 }
 
 function getCategories() {
-	categories = [ ];
-	categories.push($(".js-category").find(".select2-chosen").text());
+  categories = [ ];
+  categories.push($(".js-category").find(".select2-chosen").text());
 
-	return categories;
+  return categories;
 }
 
 function buildRequest() {
-	sendRequest(getTagsInputValues(".js-tags"), getTagsInputValues(".js-keywords"), getCategories());
+  sendRequest(getTagsInputValues(".js-tags"), getTagsInputValues(".js-keywords"), getCategories());
 }
 
 function sendRequest(tagNames, keywords, categories) {
-
-
-	$.ajax({
-	  type: "POST",
-	  url: urlLocal,
-	  data: { 
-	  	"region" : 80002,
-	  	"targets" : [ "kijiji" ],
-	  	"languages" : {
-	  		"french" : {
-				  "tags" : tagNames, 
-					"keywords" : keywords, 
-					"categories" : categories 
-				},
-				"english" : {
-				  "tags" : tagNames, 
-					"keywords" : keywords, 
-					"categories" : categories 
-				}
-			}
-		},
-	  success: updateResult,
-	  dataType: "json"
-	});
+  $.ajax({
+    type: "POST",
+    url: urlLocal,
+    data: { 
+      "region" : 80002,
+      "targets" : [ "kijiji" ],
+      "languages" : {
+	"french" : {
+	  "tags" : tagNames, 
+	  "keywords" : keywords, 
+	  "categories" : categories 
+	},
+	"english" : {
+	  "tags" : tagNames, 
+	  "keywords" : keywords, 
+	  "categories" : categories 
+	}
+      }
+    },
+    success: updateResult,
+    dataType: "json"
+  });
 }
 
