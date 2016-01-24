7e3951dfd2e8e3971d3d5c4361260683b08f2623
Charles-Antoine Chartrand <CharlesA.Chartrand@Gmail.com>
2016-01-23 22:24:42 -0500

    Added the graph

--- public/js/requestBuilder.js
+++ public/js/requestBuilder.js
@@ -28,7 +28,29 @@ function buildClarifaiData() {
 }
 
 function updateResult(data, textStatus, jqXHR) {
-	//console.log(data);
+	var myChart = $("myChart"); 
+  myChart.css("display","block");
+  var ctx = myChart.get(0).getContext("2d");
+
+  
+  var data = {
+    labels: ["1", "2", "3", "4", "5"],
+    datasets: [
+      {
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.5)",
+        strokeColor: "rgba(220,220,220,0.8)",
+        highlightFill: "rgba(220,220,220,0.75)",
+        highlightStroke: "rgba(220,220,220,1)",
+        data: [data.ranges[0].length, data.ranges[1].length, data.ranges[2].length, data.ranges[3].length, data.ranges[4].length]
+      }
+    ]
+  };
+
+var ctx = $("#myChart").get(0).getContext("2d");
+var myBarChart = new Chart(ctx).Bar(data);
+  
+
 }
 
 function sendClarifaiRequest(img) {
